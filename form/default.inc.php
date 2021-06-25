7b35d9eee16d6332ffa684a4253a814e1314f93a
Alexandra Lejarza <alexandralejarza@gmail.com>
2021-06-25 16:56:10 -0400

    added contact page and styling

---
+++ form/default.inc.php
@@ -0,0 +1,123 @@
+<style type="text/css">
+* {
+	margin: 0;
+	padding: 0;
+	border: 0;
+}
+
+article, aside, details, figcaption, figure, footer, header, main, nav, section, summary {
+    display:block;
+}
+
+body {
+  font-family: 'Quicksand', sans-serif;
+}
+
+header {
+  background: #a8314b;
+  text-align: right;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 99999;
+}
+
+header nav li {
+  display: inline-block;
+  max-width: 200px;
+  vertical-align: middle;
+  padding: 25px 0;
+}
+
+header nav a {
+  text-decoration: none;
+  color: white;
+  margin: 0 20px;
+  font-size: 20px;
+  text-align: center;
+}
+
+footer {
+  background: #cb8699;
+  color: white;
+  text-align: center;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  padding: 10px;
+  z-index: 99999;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: 'Bree Serif', serif;
+  color: #102d33;
+}
+
+h1 {
+  font-size: 25px;
+}
+
+h2 {
+  padding: 10px 0 20px 0;
+  font-size: 20px;
+}
+
+h3 {
+  font-size: 15px;
+  padding: 5px 0;
+  color: #568492;
+  font-style: italic;
+}
+
+p {
+  padding-top: 10px;
+  color: #102d33;
+}
+
+.aboutme,
+.education,
+.career,
+.portfolio {
+  background-color: #ffffff;
+  border: 1px solid black;
+  opacity: 0.8;
+  max-width: 650px;
+  line-height: 150%;
+}
+
+#container {
+  width: 100%;
+  margin: auto;
+}
+
+.inquiry {
+	top: 180px;
+}
+
+</style>
+
+
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="education.html">Education</a></li>
+      <li><a href="career.html">Career</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="contact.html">Contact Me</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="inquiry">
+  <h1>Sorry something went wrong with your submission</h1>
+  <p>Please try again</p>
+</div>
+
+
+<footer>Alexandra Lejarza 2021</footer>
